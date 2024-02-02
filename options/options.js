38cd4b2ec51fce0f2866dd3285a302fa00cc7468
Crowdbotics <team@crowdbotics.com>
2024-02-02 22:35:02 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "aged-dew-45811",
+  url: "https://aged-dew-45811.botics.co",
+  api: "https://aged-dew-45811.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
